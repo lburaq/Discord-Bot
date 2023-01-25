2f58b8a2bac9a9ee2f517907ebf22703ba24d85e
Burak <103515295+lburaq@users.noreply.github.com>
2023-01-25 18:01:32 +0300

    Add files via upload

---
+++ commands/yapayzeka.js
@@ -0,0 +1,36 @@
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const config = require("../config.json");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('yapayzeka')
+    .setDescription('Yapay zekaya soru sor')
+    .addStringOption(option => option.setName('mesaj').setDescription('Sorunu yaz').setRequired(true)),
+    async execute(client,interaction) {
+        try{
+        const { Configuration, OpenAIApi } = require("openai");
+        const configuration = new Configuration({
+            organization: config.OPENAI_ORG,
+            apiKey: config.OPENAI_KEY,
+        });
+        const openai = new OpenAIApi(configuration);
+        var mesaj = interaction.options.getString('mesaj');
+        await interaction.reply(`**[Komut işleniyor]**`);
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `ChatGPT arkadaş canlısı bir sohbet botudur.\n\
+            ChatGPT: Merhaba, nasılsın?\n\
+            ${interaction.user.username}: ${mesaj}\n\
+            ChatGPT:`,
+            temperature: 0.9,
+            max_tokens: 400,
+            stop: ["ChatGPT:"],
+        })
+        return await interaction.editReply(`${response.data.choices[0].text}`);
+    }
+    catch(err){
+        console.log(err);
+        return await interaction.editReply("Beklenmedik bir hata oluştu, tekrar deneyin!");
+    }
+    },
+}
