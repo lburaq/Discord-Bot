0d735a1d9efcf2b8e0ef11e53e5e1e46bb661bcb
Burak <103515295+lburaq@users.noreply.github.com>
2023-02-01 12:34:22 +0300

    Update destek-kapat.js

--- commands/destek-kapat.js
+++ commands/destek-kapat.js
@@ -9,7 +9,7 @@ module.exports = {
        const kanaladi = interaction.channel.name;
        if(kanaladi.substring(0,6) == "destek" && kanaladi.length > 10){
         const message = await interaction.reply({content: "**Kanal 5 saniye sonra silinecektir, engellemek için emojiye tıkla!**", fetchReply: true });
-        await message.react('⏹️');
+        message.react('⏹️');
         const {filter} = require('../index');
         message.awaitReactions({filter, time:5000}).then(collected =>{
         if(collected.size==0 || message.reactions.cache.get('⏹️').count<=1){
