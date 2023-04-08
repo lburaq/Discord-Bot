68884d41bc08899243609d89d55bc52d3d3c9bfe
Burak Geriş <103515295+lburaq@users.noreply.github.com>
2023-04-08 10:04:05 +0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,32 +1,57 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const { EmbedBuilder } = require("discord.js")
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
+const { createAudioResource } = require("@discordjs/voice");
+const ytdl = require("play-dl");
 
 module.exports = {
-	data: new SlashCommandBuilder().setName("gec").setDescription("Mevcut şarkıyı geç"),
-	async execute(client,interaction) {
-		const queue = client.player.getQueue(interaction.guildId)
-
-		if (!queue) return interaction.reply("Zaten bot çalmıyor")
-
-        var currentSong = queue.current
-
-		queue.skip()
-
-        interaction.reply({
-            embeds: [
-                new EmbedBuilder().setDescription(`${currentSong.title} Başarılı şekilde geçildi`).setThumbnail(currentSong.thumbnail).setColor("#36eaf1")
-            ]
+  data: new SlashCommandBuilder()
+    .setName("gec")
+    .setDescription("Mevcut şarkıyı geç")
+    .setDMPermission(false),
+  async execute(client, interaction) {
+    const { song, queue } = require("./play");
+    let embed = new EmbedBuilder();
+    if (!queue || !queue.has(interaction.guild.id)) {
+      return interaction.reply("Zaten bot çalmıyor");
+    }
+    const serverQueue = queue.get(interaction.guild.id);
+    serverQueue.songs.shift();
+    if (serverQueue.songs.length !== 0) {
+      let thumbnail = serverQueue.songs[0].thumbnail
+        ? serverQueue.songs[0].thumbnail
+        : serverQueue.songs[0].thumbnails[0].url;
+      await interaction.deferReply();
+      let stream = await ytdl.stream(serverQueue.songs[0].url, {
+        quality: 0,
+      });
+      const resource = createAudioResource(stream.stream, {
+        inputType: stream.type,
+      });
+      serverQueue.connection.state.subscription.player.play(resource);
+      embed
+        .setDescription(
+          `**[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})** Şu anda çalan!`
+        )
+        .setThumbnail(thumbnail ? thumbnail : song.thumbnail)
+        .setFooter({
+          text: `Video süresi: ${
+            song.duration ? song.duration : serverQueue.songs[0].durationRaw
+          }`,
         })
-        /*
-        setTimeout(()=>{
-            currentSong = queue.current
-            interaction.followUp({
-                embeds: [
-                    new EmbedBuilder().setDescription(`${currentSong.title} Şu anda çalan`).setThumbnail(currentSong.thumbnail).setColor("#36eaf1")
-                ]
-            })
-        },3000)
-        */
-
-	},
-}
+        .setColor("#36eaf1");
+      interaction.editReply({
+        content: "Başarılı şekilde geçildi!",
+        ephemeral: true,
+      });
+      serverQueue.playMessageChannel.edit({ embeds: [embed] });
+    } else {
+      queue.delete(interaction.guild.id);
+      serverQueue.connection.destroy();
+      embed.setDescription(`**Tüm şarkılar bitti!**`).setColor("#36eaf1");
+      interaction.reply({
+        content: "Listede şarkı kalmadı!",
+        ephemeral: true,
+      });
+    }
+  },
+};
