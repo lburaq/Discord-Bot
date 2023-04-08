8d9742a3c53c2396efefc64cacbdb241b192f00b
Burak Geriş <103515295+lburaq@users.noreply.github.com>
2023-04-08 10:04:55 +0300

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,13 +1,21 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-	data: new SlashCommandBuilder().setName("kapat").setDescription("Müziği tamamen kapat"),
-	async execute(client,interaction) {
-		const queue = client.player.getQueue(interaction.guildId)
-
-		if (!queue) return interaction.reply("Zaten bot çalmıyor")
-
-		queue.destroy()
-        interaction.reply({content: "Görüşürüz", ephemeral: true})
-	},
-}
+  data: new SlashCommandBuilder()
+    .setName("kapat")
+    .setDescription("Müziği tamamen kapat")
+    .setDMPermission(false),
+  async execute(client, interaction) {
+    const { queue } = require("./play");
+    if (!queue || !queue.has(interaction.guild.id)) {
+      return interaction.reply("Zaten bot çalmıyor");
+    }
+    const serverQueue = queue.get(interaction.guild.id);
+    queue.delete(interaction.guild.id);
+    serverQueue.connection.destroy();
+    interaction.reply({
+      content: "Bot Başarılı şekilde kapatıldı",
+      ephemeral: true,
+    });
+  },
+};
